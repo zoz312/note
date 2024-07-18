8ab4eca4be3ad2e1298463c364fb4ea821493215
hozifakhussouna <hdhyfhkhaldhswkhh@gmail.com>
2024-07-18 15:28:24 +0300

    Create theme and note view class

---
+++ lib/views/Note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteWiew extends StatelessWidget {
+  const NoteWiew({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
